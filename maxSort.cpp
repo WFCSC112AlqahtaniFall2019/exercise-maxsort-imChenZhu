fa5ac55cb36bc108afc39c65a5742499e552e295
zhuchen <zhuc18@wfu.edu>
2019-09-10 15:42:02 -0400

    910

--- maxSort.cpp
+++ maxSort.cpp
@@ -9,20 +9,27 @@ void print(const vector<int>& list);
 
 int main() {
 
-    vector<int> v(5);  // vector to be sorted
+    vector<int> v(5);
+
+
+    // vector to be sorted
     // initialize v
+    v = {6, 1, 3, 2, 1};
     /* your code here */
 
 
     // print the unsorted v
+    cout<<"v = {6, 1, 3, 2, 1}"<<endl;
     /* your code here */
 
 
     // sort v
+    maxSort( v);
     /* your code here */
 
 
     // print the sorted v
+    print(v);
     /* your code here */
 
 
@@ -34,23 +41,40 @@ int main() {
 //Max sort function
 void maxSort(vector<int>& list){
     for(int i=list.size()-1;i>=0;i--){
-        /*complete the code*/
+
+        int current_max = 0;
+        int currentPosition = 0;/*complete the code*/
 
         for(int j=0;j<=i;j++){
-            /*complete the code*/
+           if(list[j]>current_max){
+               current_max = list[j];
+               currentPosition = j;
+           }
+           /*complete the code*/
 
         }
+        swap(list[currentPosition], list[i]);
         /*complete the code*/
     }
 }
 
 // swap function
 void swap(int& a, int&b){
-    /*your code here*/
+    int temp = a;
+    a=b;
+    b=temp;/*your code here*/
 }
 
 // print function
 void print(const vector<int>& list){
-    /*your code here*/
+    cout<<"v = {";
+    for(int k=0;k<5;k++){
+        if(k<4){
+            cout<<list[k]<<", ";
+        }
+        else{
+            cout<<list[k]<<"}";
+        }
+    }/*your code here*/
 }
 
